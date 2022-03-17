c6712f8ace95931d0cea6e8b0974f300740dc181
PierreLGZ <50136639+PierreLGZ@users.noreply.github.com>
2022-03-17 11:42:37 +0100

    Add files via upload

---
+++ Code/controle.py
@@ -0,0 +1,103 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import sys
+import webcam4
+
+class Controle:
+
+    gc_mode = 0
+    cap = None
+
+    def __init__(self):
+        Controle.gc_mode = 1
+        Controle.cap = cv2.VideoCapture(0)
+
+    def start(self):
+        while Controle.cap.isOpened() and Controle.gc_mode:
+            # Récupération de l'image de la webcam
+            success, img = Controle.cap.read()
+
+            detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+            buttonRecFac = Button((100, 50), 500, 100, "Reconnaissance faciale")
+            buttonQuit = Button((700, 50), 300, 100, "Quitter")
+            buttonNew = Button((700, 200), 300, 100, "Nouveau Bouton")
+
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue
+
+            img = cv2.resize(img, (1024, 720), fx=0, fy=0, interpolation = cv2.INTER_CUBIC)
+            img = cv2.flip(img, 1)
+            #img = cv2.namedWindow("Image")
+            hands, img_hand = detector.findHands(img, flipType=False)
+
+            # Afficher les boutons
+            buttonRecFac.draw(img_hand)
+            buttonQuit.draw(img_hand)
+
+            if hands:
+                # Main
+                lmList = hands[0]['lmList']
+                length, _, img_dist = detector.findDistance(lmList[8], lmList[12], img) 
+                x, y = lmList[8]
+                if length < 50:
+                    if buttonRecFac.checkClick(x, y, img_dist):
+                        webcam4.launch_algo()
+                        #Controle.cap.release()
+                        cv2.imshow("Image", img)
+                        cv2.waitKey(100)
+                        Controle.cap.release()
+                        cv2.destroyAllWindows()
+                    if buttonQuit.checkClick(x, y, img_dist):
+                        sys.exit()
+
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)
+        
+        #Controle.cap.release()
+        #cv2.destroyAllWindows()
+
+class Button:
+    def __init__(self, pos, width, height, value):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.value = value
+
+    def draw(self, img):
+        cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height), 
+            (225, 225, 225), cv2.FILLED)
+
+        cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height),
+            (50, 50, 50), 3)
+        
+        cv2.putText(img, self.value, (self.pos[0]+ 40, self.pos[1] + 60), cv2.FONT_HERSHEY_PLAIN,
+            2, (50, 50, 50), 2)
+
+    def checkClick(self, x, y, img):
+        if self.pos[0] < x < self.pos[0] + self.width and self.pos[1] < y < self.pos[1] + self.height:
+            cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height), 
+                (255, 255, 255), cv2.FILLED)
+
+            cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height),
+                (50, 50, 50), 3)
+        
+            cv2.putText(img, self.value, (self.pos[0]+ 20, self.pos[1] + 70), cv2.FONT_HERSHEY_PLAIN,
+                2, (0, 0, 0), 5)
+
+            return True
+        else:
+            return False
+
+    def drawNew(self, img):
+        cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height), 
+            (225, 225, 225), cv2.FILLED)
+        
+        cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height),
+            (50, 50, 50), 3)
+    
+        cv2.putText(img, self.value, (self.pos[0]+ 40, self.pos[1] + 60), cv2.FONT_HERSHEY_PLAIN,
+            2, (50, 50, 50), 2)
+
+
---
+++ Code/natacha.py
@@ -0,0 +1,141 @@
+import pyttsx3
+import speech_recognition as sr
+from datetime import date
+import time
+import webbrowser
+import datetime
+from pynput.keyboard import Key, Controller
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+import smtplib
+import app
+from threading import Thread
+import controle
+import cv2
+
+# Intialisation des objets
+r = sr.Recognizer()
+keyboard = Controller()
+engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+# Instanciation des variables
+file_exp_status = False
+files =[]
+path = ''
+is_awake = True  # Statut du bot
+
+# Fonctions
+def reply(audio):
+    app.ChatBot.addAppMsg(audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def wish():
+    reply("Bonjour, je m'appelle Natasha, comment puis-je vous aider ?")
+
+# Paramètres du micro
+with sr.Microphone() as source:
+        r.energy_threshold = 100
+        r.dynamic_energy_threshold = False
+
+# Cast de l'audio en string
+def record_audio():
+    with sr.Microphone() as source:
+        r.pause_threshold = 0.8
+        voice_data = ''
+        audio = r.listen(source, phrase_time_limit=5)
+
+        try:
+            voice_data = r.recognize_google(audio, language='fr-FR')
+        except sr.RequestError:
+            reply("Désolé, j'ai un problème de réseau. Pouvez-vous vérifier votre connexion ?")
+        except sr.UnknownValueError:
+            print("Je ne comprends pas !")
+            pass
+        return voice_data.lower()
+
+# Exécution des commandes
+def respond(voice_data):
+    global file_exp_status, files, is_awake, path
+    voice_data.replace('natacha','')
+    app.eel.addUserMsg(voice_data)
+
+    if is_awake==False:
+        if 'réveille-toi' in voice_data:
+            is_awake = True
+            wish()
+
+    # Contrôles simples
+    elif 'bonjour' in voice_data:
+        wish()
+
+    elif 'dédicace' in voice_data:
+        reply("Ricco est notre héros !")
+
+    elif ('bye' in voice_data) or ('by' in voice_data):
+        reply("A plus tard mes B.G.!")
+        is_awake = False
+
+    elif ('merci' in voice_data):
+        if controle.Controle.gc_mode:
+            controle.Controle.gc_mode = 0
+        app.ChatBot.close()
+        cv2.waitKey(10)
+        cv2.destroyAllWindows()
+        sys.exit()
+        
+    # Contrôles dynamiques
+    elif 'contrôle' in voice_data:
+        if controle.Controle.gc_mode:
+            reply("Vous avez déjà le contrôle")
+        else:
+            c = controle.Controle()
+            t = Thread(target = c.start)
+            t.start()
+            reply("Lancement en cours !")
+
+    elif 'stop' in voice_data:
+        if controle.Controle.gc_mode:
+            controle.Controle.gc_mode = 0
+            reply("Fermeture de l'application")
+        else:
+            reply("L'application est déjà inactive")
+                   
+    else: 
+        reply("Je ne suis pas programmée pour faire ça !")
+
+# Exécution du chatbot
+t1 = Thread(target = app.ChatBot.start)
+t1.start()
+
+while not app.ChatBot.started:
+    time.sleep(0.5)
+
+wish()
+voice_data = None
+while True:
+    if app.ChatBot.isUserInput():
+        # Récupération de la saisie clavier
+        voice_data = app.ChatBot.popUserInput()
+    else:
+        # Récupération du vocal
+        voice_data = record_audio()
+
+    # Reconnaissance vocale
+    if 'natacha' in voice_data:
+        try:
+            respond(voice_data)
+        except SystemExit:
+            reply("A bientôt")
+            break
+        except:
+            print("Une exception s'est produite pendant la fermeture.") 
+            break
+        
+
+
